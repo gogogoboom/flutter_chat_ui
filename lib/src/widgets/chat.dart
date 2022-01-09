9d2f9fc96cf670d267e3dc0be5cf0d9e6b07a273
jessezhang <529840155@qq.com>
2022-01-10 00:12:33 +0800

    chat

--- lib/src/widgets/chat.dart
+++ lib/src/widgets/chat.dart
@@ -457,7 +457,17 @@ class _ChatState extends State<Chat> {
             alignment: Alignment.center,
             children: [
               Container(
-                color: widget.theme.backgroundColor,
+                // color: widget.theme.backgroundColor,
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                      colors: [
+                        Color(0XFFE1E2F5),
+                        Color(0xfffee7ed),
+                      ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter
+                  )
+                ),
                 child: Column(
                   children: [
                     Flexible(
